c3d4fcf44ca320566002ee0ae1a29fd9b35e9c9b
Jydis <Joristdh@gmail.com>
2017-04-04 13:21:52 +0200

    Sidebar back function works, placement button should be changed

--- js/scripts.js
+++ js/scripts.js
@@ -4,29 +4,30 @@ $(document).ready(function () {
     $(".arrowc").rotate({
         angle: 180
     });
-     $("#wrapper.home").toggleClass("toggled");
+    $("#wrapper.home").toggleClass("toggled");
     if (window.innerWidth < 768) {
         $("#wrapper").toggleClass("toggled");
     }
-   
-    
-
+    $("#back").click(function () {
+        $("#wrapper.toggled #sidebar-wrapper.CSE").css("padding-left", "250px");
+        $("#wrapper.toggled #sidebar-wrapper.main").css("width", "250px");
+    });
 
     /* $(function () {
-        $("#pagebody").load("docs/setup_survey.html");
-    });
+            $("#pagebody").load("docs/setup_survey.html");
+        });
 
-    $("#general").click(function () {
-        $(function () {
-            $("#pagebody").load("docs/general_learningobj.html");
+        $("#general").click(function () {
+            $(function () {
+                $("#pagebody").load("docs/general_learningobj.html");
+            });
         });
-    });
-    $("#bachelor").click(function () {
-        $(function () {
-            $("#pagebody").load("docs/general_aboutcse.html");
+        $("#bachelor").click(function () {
+            $(function () {
+                $("#pagebody").load("docs/general_aboutcse.html");
+            });
         });
-    });
-*/
+    */
 
 
     $(".fold").click(function () {
